80a2f7366af51043af38b342275f21e82bf7790b
T-RohanDesigns-dot <rohangupta3634@gmail.com>
2026-02-03 16:54:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,35 @@ upload.addEventListener("change", (e) => {
   };
   img.src = URL.createObjectURL(file);
 });
+function increaseBrightness() {
+  adjustBrightness(10);
+}
+
+function decreaseBrightness() {
+  adjustBrightness(-10);
+}
+
+function adjustBrightness(value) {
+  const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imgData.data;
+
+  for (let i = 0; i < data.length; i += 4) {
+    data[i] += value;     // R
+    data[i + 1] += value; // G
+    data[i + 2] += value; // B
+  }
+  ctx.putImageData(imgData, 0, 0);
+}
+
+function sharpenImage() {
+  ctx.filter = "contrast(120%) saturate(110%)";
+  ctx.drawImage(canvas, 0, 0);
+  ctx.filter = "none";
+}
+
+function downloadImage() {
+  const link = document.createElement("a");
+  link.download = "pixelease.png";
+  link.href = canvas.toDataURL();
+  link.click();
+}
