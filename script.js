d5cb8095be87ff5fab32888ab022a8d42f0a7d2b
T-RohanDesigns-dot <rohangupta3634@gmail.com>
2026-02-04 13:54:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,24 +19,30 @@ function resetImage() {
   ctx.drawImage(originalImage, 0, 0);
   historyStack = [];
 }
-const upload = document.getElementById("upload");
+const uploadInput = document.getElementById("upload");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 let originalImage = null;
 
-upload.addEventListener("change", (e) => {
-  const file = e.target.files[0];
+uploadInput.addEventListener("change", function () {
+  const file = this.files[0];
   if (!file) return;
 
   const img = new Image();
-  img.onload = () => {
-    originalImage = img;
+  img.onload = function () {
     canvas.width = img.width;
     canvas.height = img.height;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(img, 0, 0);
-    historyStack = [];
+
+    originalImage = img;
   };
+
+  img.src = URL.createObjectURL(file);
+});
+
   img.src = URL.createObjectURL(file);
 });
 
